f289a5285883a5b5de9fb6b22786920d03ccc9af
TsAlexandr <lunoface@mail.ru>
2022-04-13 23:35:10 +0700

    add client mongo

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,3 +1,28 @@
+import {MongoClient} from 'mongodb'
+
+const mongoUri =
+    process.env.mongoURI = "mongodb://localhost:27017/?maxPoolSize=20&w=majority";
+
+export const client = new MongoClient(mongoUri);
+
+export async function runDb() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+        // Establish and verify connection
+        await client.db("bloggers - posts").command({ ping: 1 });
+        console.log("Connected successfully to mongo server");
+
+    } catch {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+export const bloggersCollection = client.db().collection('bloggers-management')
+
+
+
 interface PostsCon {
     id: number
     bloggerId?: Bloggers['id'],
