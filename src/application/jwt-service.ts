e03bb1e2314134e0511d8693ac50ceb7fe9100ab
Alex <lunoface@mail.ru>
2022-04-23 04:48:46 +0700

    add the authorization and authentication

---
+++ src/application/jwt-service.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import {ObjectId} from "mongodb";
+import {settings} from "../settings";
+import {Users} from "../repositories/db";
+
+
+
+export const jwtService = {
+    async createJWT(user: Users) {
+        const token = jwt.sign({userId: user._id}, settings.JWT_SECRET, {expiresIn: '1h'})
+        return token
+    },
+    async getUserIdByToken(token: string) {
+        try {
+            const result: any = jwt.verify(token, settings.JWT_SECRET)
+            return new ObjectId(result.userId)
+        } catch (error) {
+            return null
+        }
+    }
+}
