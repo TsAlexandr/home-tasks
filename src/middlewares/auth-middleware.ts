e14898204664aab92f4ad2910ac0731528b1bae5
Alex <lunoface@mail.ru>
2022-04-23 04:43:58 +0700

    create auth-middleware.ts for authorization by token

---
+++ src/middlewares/auth-middleware.ts
@@ -0,0 +1,21 @@
+import {NextFunction, Request, Response} from "express";
+import {jwtService} from "../application/jwt-service";
+import {usersService} from "../domain/users-service";
+
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization) {
+        res.send(401)
+        return
+    }
+
+
+    const token = req.headers.authorization.split(' ')[1]
+
+    const userId = await jwtService.getUserIdByToken(token)
+    if (userId) {
+        req.user = await usersService.findUserById(userId)
+        next()
+    }
+    res.send(401)
+}
