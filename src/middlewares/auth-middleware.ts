dca098288fd23910d7ccbf20a52cf873d18e8d50
Alex <lunoface@mail.ru>
2022-05-04 12:33:02 +0700

    first commit

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    if (req.query.admin === 'qwerty') {
+    if (req.headers.admin === 'admin' && req.headers.password === 'qwerty') {
         next()
     } else {
         res.status(401)
