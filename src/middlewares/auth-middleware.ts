70982119cd9bee39e1bd2a2fc89b66f556431118
Alex <lunoface@mail.ru>
2022-05-05 21:28:55 +0700

    first commit

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -2,7 +2,12 @@ import {NextFunction, Request, Response} from "express";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization
-    const auth = Buffer.from(authHeader!.split(' ')[1], 'base64').toString().split(':')
+    if(!authHeader){
+        return
+}else if(!authHeader.split(' ')[1] ||  authHeader.split(' ')[1]=='admin:qwerty' || authHeader.split(' ')[0]!= 'Basic') {
+            res.status(401)
+        }
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
     const login = auth[0]
     const password = auth[1]
     if (login === 'admin' && password === 'qwerty') {
