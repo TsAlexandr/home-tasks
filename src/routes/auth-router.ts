89a0577c81827f42b6a605d1a66772d83b2578e6
Alex <lunoface@mail.ru>
2022-05-12 16:42:18 +0700

    n/s

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,16 +1,16 @@
-// import {Router, Request, Response} from "express";
-// import {usersService} from "../domain/users-service";
-// import {jwtService} from "../application/jwt-service";
-//
-//
-// export const authRouter = Router({})
-//
-// authRouter.post('/login', async (req: Request, res: Response) => {
-//     const user = await usersService.checkCredentials(req.body.login, req.body.password)
-//         if(user) {
-//             const token = await jwtService.createJWT(user)
-//             res.status(201).send(token)
-//         } else {
-//             res.sendStatus(401)
-//         }
-// })
+import {Router, Request, Response} from "express";
+import {usersService} from "../domain/users-service";
+import {jwtService} from "../application/jwt-service";
+
+
+export const authRouter = Router({})
+
+authRouter.post('/login', async (req: Request, res: Response) => {
+    const user = await usersService.checkCredentials(req.body.login, req.body.password)
+        if(user) {
+            const token = await jwtService.createJWT(user)
+            res.status(201).send(token)
+        } else {
+            res.sendStatus(401)
+        }
+})
