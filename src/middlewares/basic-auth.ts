119906006bc20d8b0881c9369df54699917245f9
Alex <lunoface@mail.ru>
2022-05-12 16:45:42 +0700

    refactor getting auth

--- src/middlewares/basic-auth.ts
+++ src/middlewares/basic-auth.ts
@@ -1,58 +1,34 @@
 import {Request, Response, NextFunction} from "express";
 
-interface BaseAuthData {
-    login: string;
-    password: string;
-}
-
-enum BaseAuthPayload {
-    login = "admin",
-    password = "qwerty",
+enum DataAdmin {
+    login = 'admin',
+    password = 'qwerty'
 }
 
 
 
 export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
     if (!req.headers.authorization) {
-        res.send(401);
+        res.sendStatus(401);
         return;
     }
 
     if (req.headers.authorization.split(" ")[0] !== "Basic") {
-        res.send(401);
+        res.sendStatus(401);
         return;
     }
 
     const token = req.headers.authorization.split(" ")[1];
-
     const decodedBaseData = authService.decodeBaseAuth(token);
 
     if (
-        (decodedBaseData.login !== BaseAuthPayload.login,
-        decodedBaseData.password !== BaseAuthPayload.password)
+        (decodedBaseData.login !== DataAdmin.login &&
+            decodedBaseData.password !== DataAdmin.password)
     ) {
-        res.send(401);
+        res.sendStatus(401);
         return;
     }
 
     next();
-
-
-
 }
 
-const authService = {
-
-    decodeBaseAuth(token: string): BaseAuthData {
-        const buff = Buffer.from(token, "base64");
-
-        const decodedString = buff.toString("ascii");
-
-        const loginAndPassword = decodedString.split(":");
-
-        return {
-            login: loginAndPassword[0],
-            password: loginAndPassword[1],
-        };
-    }
-}
