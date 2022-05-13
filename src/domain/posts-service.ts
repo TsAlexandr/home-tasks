177a3763a41734e8f17a08a650faaedc051e37f7
Alex <lunoface@mail.ru>
2022-05-13 13:55:35 +0700

    fix pageNumber, add pages service

--- src/domain/posts-service.ts
+++ src/domain/posts-service.ts
@@ -4,8 +4,8 @@ import {bloggersService} from "./bloggers-service";
 
 
 export const postsService = {
-    async getPosts(pageNumber: number, pageSize: number) {
-        return await postsRepository.getPosts(pageSize, pageNumber)
+    async getPosts(page: number, pageSize: number) {
+        return await postsRepository.getPosts( page, pageSize )
     },
     async getPostsById(id: number) {
         return await postsRepository.getPostsById(id)
@@ -13,12 +13,9 @@ export const postsService = {
     async deletePostsById(id: number) {
         return await postsRepository.deletePostsById(id)
     },
-    async updatePostsById(id: number, isUpdPost: PostsCon) {
-            const updPost = {
-                ...isUpdPost,
-                id
-            }
-            return await postsRepository.updatePostsById(updPost)
+    async updatePostsById(id: number, name: string, updatePost: NewPost) {
+            const bloggerName = name
+            return await postsRepository.updatePostsById({id, bloggerName, ...updatePost})
     },
     async createPosts(newPost: NewPost) {
         const blogger = await bloggersService.getBloggersById(newPost.bloggerId)
@@ -32,6 +29,9 @@ export const postsService = {
         }
         return await postsRepository.createPosts(createPost)
     },
-
+    async getPostsInPages(bloggerId: number, page: number, pageSize: number) {
+        const post = await postsRepository.getPostInPages(bloggerId, page, pageSize)
+        return post
+    }
 
 }
