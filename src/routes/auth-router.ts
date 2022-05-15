9a3c4f54e5693db97549eb826edb5c8eb7051490
Alex <lunoface@mail.ru>
2022-05-15 23:33:43 +0700

    change using route

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,12 +1,13 @@
 import {Router, Request, Response} from "express";
 import {usersService} from "../domain/users-service";
 import {jwtService} from "../application/jwt-service";
+import {authService} from "../domain/auth-service";
 
 
 export const authRouter = Router({})
 
 authRouter.post('/login', async (req: Request, res: Response) => {
-    const user = await usersService.checkCredentials(req.body.login, req.body.password)
+    const user = await authService.checkCredentials(req.body.login, req.body.password)
         if(user) {
             const token = await jwtService.createJWT(user)
             res.status(201).send(token)
