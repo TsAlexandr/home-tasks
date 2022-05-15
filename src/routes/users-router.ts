8f31f56b1436cbc5d5085627ac8a626c73595736
Alex <lunoface@mail.ru>
2022-05-15 23:37:51 +0700

    add getById route, fix status and authorization

--- src/routes/users-router.ts
+++ src/routes/users-router.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response} from "express";
 import {usersService} from "../domain/users-service";
 import {authMiddleware} from "../middlewares/auth-middleware";
 import {getPage, inputValidator} from "../middlewares/input-validator-middlewares";
+import {checkAuth} from "../middlewares/basic-auth";
 
 
 export const usersRouter = Router({})
@@ -15,38 +16,30 @@ usersRouter.get('/',
         res.status(200).send(users)
     })
 
-    .get('/:id', inputValidator,
-    async (req: Request, res: Response) => {
-        const id = req.params.id
-        const user = await usersService.findUserById(id)
-
-    })
-
     .post('/',
+        checkAuth,
         inputValidator,
-        authMiddleware,
         async (req: Request, res: Response) => {
             const newUser = await usersService.createUser(
-                req.body.username,
+                req.body.login,
                 req.body.password
             )
             if (newUser) {
                 res.status(201).send(newUser)
             } else {
-                res.status(400)
-
+                res.sendStatus(400)
             }
         })
 
-    .delete('/:id',
+    .delete('/:userId',
+        checkAuth,
         inputValidator,
-        authMiddleware,
         async (req: Request, res: Response) => {
-            const id = req.body.ObjectId
-            const delUser = await usersService.deleteUser(id)
+            const userId = req.body.userId
+            const delUser = await usersService.deleteUser(userId)
             if (!delUser) {
-                res.status(401)
+                res.sendStatus(404)
             } else {
-                res.status(204)
+                res.sendStatus(204)
             }
         })
