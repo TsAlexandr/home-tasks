bf10c06e59862f1f6c2dcf83ab8fad56ebac8460
Alex <lunoface@mail.ru>
2022-05-15 22:26:55 +0700

    refactor

--- src/routes/users-router.ts
+++ src/routes/users-router.ts
@@ -1,49 +1,52 @@
 import {Router, Request, Response} from "express";
 import {usersService} from "../domain/users-service";
 import {authMiddleware} from "../middlewares/auth-middleware";
-import {inputValidator} from "../middlewares/input-validator-middlewares";
-import {getPageFor} from "./bloggers-router";
+import {getPage, inputValidator} from "../middlewares/input-validator-middlewares";
+
 
 export const usersRouter = Router({})
 
 
 usersRouter.get('/',
     inputValidator,
-    authMiddleware,
     async (req: Request, res: Response) => {
-    const {pageNumber, pageSize} = getPageFor(req.query)
-    const users = usersService.getUsers(pageNumber,pageSize)
-        if(!users) {
-            res.sendStatus(400)
-        } else {
-            res.send(users)
-        }
-})
+        const {page, pageSize} = getPage(req.query)
+        const users = usersService.getUsers(page, pageSize)
+        res.status(200).send(users)
+    })
+
+    .get('/:id', inputValidator,
+    async (req: Request, res: Response) => {
+        const id = req.params.id
+        const user = await usersService.findUserById(id)
+
+    })
+
     .post('/',
         inputValidator,
         authMiddleware,
-        async(req: Request, res: Response) => {
-        const newUser = await usersService.createUser(
-            req.body.username,
-            req.body.password
-        )
-        if (newUser) {
-            res.status(201).send(newUser)
-        } else {
-            res.status(400)
+        async (req: Request, res: Response) => {
+            const newUser = await usersService.createUser(
+                req.body.username,
+                req.body.password
+            )
+            if (newUser) {
+                res.status(201).send(newUser)
+            } else {
+                res.status(400)
 
-        }
-    })
+            }
+        })
 
     .delete('/:id',
         inputValidator,
         authMiddleware,
         async (req: Request, res: Response) => {
-        const id = req.body.ObjectId
-        const delUser = await usersService.deleteUser(id)
-            if(!delUser) {
+            const id = req.body.ObjectId
+            const delUser = await usersService.deleteUser(id)
+            if (!delUser) {
                 res.status(401)
             } else {
                 res.status(204)
             }
-    })
+        })
