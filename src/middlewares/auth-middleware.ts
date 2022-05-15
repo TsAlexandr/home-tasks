0d4dab123e867228a986b425cf26287dce51d326
Alex <lunoface@mail.ru>
2022-05-15 22:18:10 +0700

    add check the headers, fix status

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,21 +1,32 @@
 import {NextFunction, Request, Response} from "express";
 import jwt from "jsonwebtoken";
 import {usersRepo} from "../repositories/users-repo";
-import {ObjectId} from "mongodb";
+import {Users} from "../repositories/db";
 
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     if (!req.headers.authorization) {
-        res.send(401)
+        res.sendStatus(401)
         return
     }
+
     const token = req.headers.authorization.split(' ')[1]
+    const version = req.headers.authorization.split(' ')[0]
+    if(version !== 'Bearer') {
+        res.sendStatus(401)
+        return
+    }
     try {
-        const decode = jwt.verify(token, 'secret')
-        const user = await usersRepo.findUserById(new ObjectId(decode.userId))
+        const decode: any = jwt.verify(token, 'secret')
+        const user = await usersRepo.findById(decode.userId)
+        if(!user) {
+            res.sendStatus(404)
+            return
+        }
         req.user = user
     } catch (e) {
-        res.status(401)
+        res.sendStatus(401)
+        return
     }
     next()
 }
