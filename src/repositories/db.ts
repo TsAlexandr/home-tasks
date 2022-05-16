c031e3150b59878f3f62ced9a714da25ac90a01f
Alex <lunoface@mail.ru>
2022-05-16 22:36:03 +0700

    fix pagination

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,6 +1,7 @@
 import {MongoClient} from 'mongodb'
 
 const mongoUri = process.env.MONGO_URI || "mongodb+srv://hello:rerere@cluster0.rxylv.mongodb.net/Cluster0?retryWrites=true&w=majority"
+|| "mongodb://localhost:27017/?maxPoolSize=20&w=majority"
 
 export const client = new MongoClient(mongoUri);
 
@@ -9,7 +10,7 @@ export const postsCollection = client.db("bloggers-posts").collection<PostsCon>(
 export const usersCollection = client.db("bloggers-posts").collection<Users>('users')
 export const commentsCollection = client.db("bloggers-posts").collection<Comment>('comments')
 
-//"mongodb://localhost:27017/?maxPoolSize=20&w=majority"
+//
 
 export async function runDb() {
     try {
