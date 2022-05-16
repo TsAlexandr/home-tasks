54940af4792399ecfe9df8c6bfb348ab3db720a8
Alex <lunoface@mail.ru>
2022-05-16 14:54:58 +0700

    fix pagination

--- src/repositories/users-repo.ts
+++ src/repositories/users-repo.ts
@@ -3,10 +3,10 @@ import {Paginator, Users, usersCollection} from "./db";
 
 export const usersRepo = {
     async getUsers(page: number, pageSize: number) {
-        const user = await usersCollection
-            .find({})
-            .skip((page - 1) * pageSize)
+        const user: Users[] = await usersCollection
+            .find({}, {projection: {_id: 0}})
             .limit(pageSize)
+            .skip((page - 1) * pageSize)
             .toArray()
         const total = await usersCollection.countDocuments({})
         const pages = Math.ceil(total / pageSize)
