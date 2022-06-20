76e20960661dc283688757b78da0d4abea308c3d
Alex <lunoface@mail.ru>
2022-06-20 14:36:15 +0700

    set proxy, add delete method for tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-
 import {runDb} from "./repositories/db";
 import {postsRouter} from "./routes/posts-router";
 import {bloggersRouter} from "./routes/bloggers-router";
@@ -9,14 +8,13 @@ import {commentsRouter} from "./routes/comments-router";
 import {usersRouter} from "./routes/users-router";
 import {authRouter} from "./routes/auth-router";
 import {emailRouter} from "./routes/email-router";
-
+import {removeAll} from "./application/autotests";
 const app = express()
 const port = process.env.PORT || 5000
 
+app.set('trust proxy', true)
 app.use(bodyParser.json())
 app.use(cors())
-
-
 app.use('/posts', postsRouter)
 app.use('/bloggers', bloggersRouter)
 app.use('/comments', commentsRouter)
@@ -24,8 +22,9 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 app.use('/email', emailRouter)
 
-
-
+app.delete('/api/testing/all-data', (req: Request, res: Response) => {
+    removeAll().then(()=> res.sendStatus(204))
+})
 
 const startApp = async () => {
     await runDb()
