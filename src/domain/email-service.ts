44c8ea84e3cc2d678a6c10e800a351c8941a6e0e
Alex <lunoface@mail.ru>
2022-06-20 14:35:31 +0700

    use inversify, add template

--- src/domain/email-service.ts
+++ src/domain/email-service.ts
@@ -1,12 +1,23 @@
 import nodemailer from 'nodemailer'
+import {injectable} from "inversify";
+import {EmailConfirmType, emailType} from "../repositories/db";
+import {EmailScheduler} from "../application/email-scheduler";
+import {emailScheduler, notificationRepository} from "../iocContainer";
 
-export const emailService = {
+export const templateService = {
+    getConfirmMessage(confirmationCode: string) {
+        return `<a href="https://homework00001.herokuapp.com/api/auth/registration-confirmation?confirmCode=${confirmationCode}">Confirm your email</a>`
+    }
+}
+
+@injectable()
+export class EmailService {
     async sendEmail(email: string, message: string, subject: string) {
         let transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user: 'gerberalex210@gmail.com', // generated ethereal user
-                pass: 'blbyf[eq15', // generated ethereal password
+                user: process.env.EMAIL_LOGIN, // generated ethereal user
+                pass: process.env.EMAIL_PASS, // generated ethereal password
             },
         });
 
@@ -18,5 +29,10 @@ export const emailService = {
             html: message, // html body
         });
         return info
+    }
+    async addMessageInQueue(message: emailType) {
+        const result = await notificationRepository.enqueueMessage(message)
+        if(result) await emailScheduler.emailSend()
+        return result
     }
 }
