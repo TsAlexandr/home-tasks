8ba1aba3574ea7efa3f7909682320624f9da04f6
Alex <lunoface@mail.ru>
2022-06-20 14:24:19 +0700

    use inversify, refactor code for email

--- src/domain/auth-service.ts
+++ src/domain/auth-service.ts
@@ -1,23 +1,27 @@
-import {usersRepo} from "../repositories/users-repo";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import {BaseAuthData} from "../repositories/db";
 import {ObjectId} from "mongodb";
 import {v4} from "uuid";
 import add from 'date-fns/add'
+import {injectable} from "inversify";
+import {isAfter} from "date-fns";
+import {EmailService, templateService} from "./email-service";
+import {emailService, usersRepository} from "../iocContainer";
 
+@injectable()
+export class AuthService {
 
-export const authService = {
-    async checkCredentials(login: string, password: string){
-        const user: any = await usersRepo.findByLogin(login)
-        if(!user) return {
+    async checkCredentials(login: string, password: string) {
+        const user: any = await usersRepository.findByLogin(login)
+        if (!user) return {
             resultCode: 1,
             data: {
                 token: null
             }
         }
         const isItHash = await this._correctHash(password, user.passwordHash)
-        if(!isItHash) {
+        if (!isItHash) {
             return {
                 resultCode: 1,
                 data: {
@@ -34,15 +38,15 @@ export const authService = {
             }
         }
 
-    },
+    }
     async _generateHash(password: string) {
         const hash = await bcrypt.hash(password, 10)
         return hash
-    },
+    }
     async _correctHash(password: string, hash: string) {
         const equal = await bcrypt.compare(password, hash)
         return equal
-    },
+    }
     decodeBaseAuth(token: string): BaseAuthData {
         const buff = Buffer.from(token, "base64");
 
@@ -53,31 +57,34 @@ export const authService = {
             login: loginAndPassword[0],
             password: loginAndPassword[1],
         };
-    },
-    async createUser(login: string, email: string, password: string) {
-        const passwordHash = await this._generateHash(password)
-        const user: UserAccountType = {
-            _id: new ObjectId(),
-            accountData: {
-                username: login,
-                email,
-                passwordHash,
-                createdAt: new Date()
-            },
-            emailConfirm: {
-                confirmationCode: v4(),
-                expirationData: add(new Date(), {
-                    hours: 1
-                }),
-                isConfirmed: false
-            }
-        }
-        const createResult = usersRepo.createUser(user)
-        await emailManager.sendEmailConfirmationMessage(user)
-        return createResult
-    },
-    async confirmEmail(login:string) {
-        let user = await usersRepo.findByLogin(login)
+    }
+    async confirmEmail(code:string) {
+        let user = await usersRepository.findByConfirmCode(code)
         if(!user) return false
+        const dbConfirmCode = user.emailConfirm.confirmationCode
+        const expired = isAfter(user.emailConfirm.expirationDate, new Date())
+        if(dbConfirmCode === code && expired) {
+            const result = await usersRepository.updateConfirm(user.accountData.id)
+            return result
+        }
+        return false
+    }
+    async resendRegistrationCode(email:string) {
+        let user = await usersRepository.findByEmail(email)
+        if(!user) return false
+        const updUser = await usersRepository.updateConfirm(user.accountData.id)
+        if(updUser) {
+            const message = templateService.getConfirmMessage(updUser.emailConfirm.confirmationCode)
+            await emailService.addMessageInQueue({
+                email: updUser.accountData.email,
+                message: message,
+                subject: 'Confirm your email',
+                isSent: false,
+                createdAt: new Date()
+            })
+            return true
+        }
+        return false
     }
 }
+
