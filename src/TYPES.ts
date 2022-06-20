6825dd18586c2adca54c0f2e3fddbaf2df0106ae
Alex <lunoface@mail.ru>
2022-06-20 14:40:54 +0700

    add types for container

---
+++ src/TYPES.ts
@@ -0,0 +1,9 @@
+export const TYPES = {
+    IBloggersRepository: Symbol.for('IBloggersRepository'),
+    BloggersService: Symbol.for('BloggersService'),
+    bloggersCollection: Symbol.for('bloggersCollection'),
+    BloggersRepository: Symbol.for('BloggersRepository'),
+    AttemptsRepository: Symbol.for('AttemptsRepository'),
+    IAttemptsRepository: Symbol.for('IAttemptsRepository'),
+    attemptsCollection: Symbol.for('attemptsCollection'),
+}
