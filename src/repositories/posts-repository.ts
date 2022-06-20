2a35ff469a7ea91fea21114aabebe365802c80d9
Alex <lunoface@mail.ru>
2022-06-20 14:38:29 +0700

    use ioc, fix pagination

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -1,6 +1,16 @@
-import {Paginator, postsCollection, PostsCon} from "./db";
+import {Bloggers, Paginator, postsCollection, PostsCon} from "./db";
+import {injectable} from "inversify";
+import * as MongoClient from 'mongodb';
+import {BloggersRepository} from "./bloggers-repository";
+import {IPostsRepository} from "../domain/posts-service";
 
-export const postsRepository = {
+
+@injectable()
+export class PostsRepository implements IPostsRepository {
+    constructor(private postsCollection: MongoClient.Collection<PostsCon>,
+                private bloggersCollection: MongoClient.Collection<Bloggers>,
+                private bloggersRepository: BloggersRepository) {
+    }
     async getPosts(page: number, pageSize: number) {
         const post: PostsCon[] = await postsCollection
             .find({}, {projection: {_id: 0}})
@@ -10,33 +20,46 @@ export const postsRepository = {
         const total = await postsCollection.countDocuments({})
         const pages = Math.ceil(total / pageSize)
 
-        const postInPages: Paginator<PostsCon> = {
+        return ({
             pagesCount: pages,
             page: page,
             pageSize: pageSize,
             totalCount: total,
             items: post
-        }
-        return postInPages
-    },
+        })
+    }
     async getPostsById(id: string) {
-        return await postsCollection.findOne({id}, {projection: {_id: 0}})
+        const post = await this.postsCollection.findOne({id}, {projection: {_id: 0}})
+        if (!post) return false
+        const blogger = await this.bloggersRepository.getBloggersById(post.bloggerId)
+        if (!blogger) return false
+        const bloggerName = blogger.name
+        return ({
+            id: post.id,
+            title: post.title,
+            shortDescription: post.shortDescription,
+            content: post.content,
+            bloggerId: post.bloggerId,
+            bloggerName
+        })
 
-
-    },
+    }
     async deletePostsById(id: string) {
         const delPost = await postsCollection.deleteOne({id})
         return delPost.deletedCount === 1
-    },
+    }
     async updatePostsById(updatePost: PostsCon) {
         const id = updatePost.id
-        const updPosts = await postsCollection.findOneAndUpdate({id}, {$set: {...updatePost}}, {upsert: true})
-        return updPosts.value
-    },
-    async createPosts(createPost: PostsCon) {
-        await postsCollection.insertOne(createPost, {forceServerObjectId: true})
-        return createPost
-    },
+        const updPosts = await postsCollection.updateOne({id}, {$set: {...updatePost}}, {upsert: true})
+        return updPosts.modifiedCount === 1
+    }
+    async createPosts(createPost: PostsCon): Promise<PostsCon | null> {
+        const blogger = await this.bloggersCollection.findOne({id: createPost.bloggerId})
+        if (!blogger) return null
+        await postsCollection.insertOne({...createPost, bloggerName: blogger.name})
+        const post = await postsCollection.findOne({id: createPost.id}, {projection: {_id: 0}})
+        return post
+    }
     async getPostInPages(bloggerId: string, page: number, pageSize: number) {
         const postsByBloggerId = await postsCollection
             .find({bloggerId}, {projection: {_id: 0}})
@@ -46,13 +69,12 @@ export const postsRepository = {
         const total = await postsCollection.countDocuments({bloggerId})
         const pages = Math.ceil(total / page)
 
-        const postInPages: Paginator<PostsCon> = {
+        return ({
             pagesCount: pages,
             page: pageSize,
             pageSize: page,
             totalCount: total,
             items: postsByBloggerId
-        }
-        return postInPages
+        })
     }
 }
