fc632f8215c910ece4a0c760e42ad651ec9ea77c
Alex <lunoface@mail.ru>
2022-06-21 02:36:53 +0700

    use yandex 10/11

--- src/domain/email-service.ts
+++ src/domain/email-service.ts
@@ -12,7 +12,7 @@ export const templateService = {
 export class EmailService {
     async sendEmail(email: string, subject: string, message: string) {
         let transporter = nodemailer.createTransport({
-            service: 'gmail',
+            service: "Yandex",
             auth: {
                 user: process.env.EMAIL_LOGIN, // generated ethereal user
                 pass: process.env.EMAIL_PASS, // generated ethereal password
