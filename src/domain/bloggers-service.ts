d5a854f8d4ed3488d8bba96a2e4fa0a09108908b
Alex <lunoface@mail.ru>
2022-06-20 14:30:03 +0700

    use ioc, use dependency inversion

--- src/domain/bloggers-service.ts
+++ src/domain/bloggers-service.ts
@@ -1,20 +1,32 @@
-import {bloggersRepository} from "../repositories/bloggers-repository";
 import {v4} from "uuid";
+import {bloggersRepository} from "../iocContainer";
+import {BloggersRepository} from "../repositories/bloggers-repository";
+import {inject, injectable} from "inversify";
+import {TYPES} from "../TYPES";
+import {Bloggers, Paginator} from "../repositories/db";
+
+@injectable()
+export class BloggersService {
+    constructor(@inject<IBloggersRepository>(TYPES.IBloggersRepository) private bloggersRepository: BloggersRepository) {
+    }
 
 
-export const bloggersService = {
     async getBloggers(page: number, pageSize: number, searchNameTerm: string) {
         return await bloggersRepository.getBloggers(page, pageSize, searchNameTerm)
-    },
+    }
+
     async getBloggersById(id: string) {
         return await bloggersRepository.getBloggersById(id)
-    },
+    }
+
     async deleteBloggerById(id: string) {
-        return  await bloggersRepository.deleteBloggerById(id)
-    },
+        return await bloggersRepository.deleteBloggerById(id)
+    }
+
     async updateBloggerById(id: string, name: string, youtubeUrl: string) {
         return await bloggersRepository.updateBloggerById(id, name, youtubeUrl)
-    },
+    }
+
     async createBlogger(name: string, youtubeUrl: string) {
         const newBlogger = {
             id: v4(),
@@ -24,5 +36,14 @@ export const bloggersService = {
         return await bloggersRepository.createBlogger(newBlogger)
     }
 }
+
+export interface IBloggersRepository {
+    getBloggers(page: number, pageSize: number, searchNameTerm: string): Promise<Paginator<Bloggers[]>>
+    getBloggersById(id: string): Promise<Bloggers | null>
+    deleteBloggerById(id: string): Promise<boolean>
+    updateBloggerById(id: string, name: string, youtubeUrl: string): Promise<Bloggers | null>
+    createBlogger(newBlogger: Bloggers): Promise<Bloggers>
+}
+
 
 
