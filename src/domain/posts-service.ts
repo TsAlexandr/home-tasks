a7c34ccf761a445100d5c5488cafb33bd131afa5
Alex <lunoface@mail.ru>
2022-06-20 14:39:55 +0700

    use ioc, dependency inversion

--- src/domain/posts-service.ts
+++ src/domain/posts-service.ts
@@ -1,38 +1,45 @@
-import {postsRepository} from "../repositories/posts-repository";
-import {NewPost} from "../repositories/db";
-import {bloggersService} from "./bloggers-service";
+import {PostsRepository} from "../repositories/posts-repository";
+import {NewPost, Paginator, PostsCon} from "../repositories/db";
 import {v4} from "uuid";
+import {injectable} from "inversify";
+import {bloggersService} from "../iocContainer";
 
+@injectable()
+export class PostsService {
+    constructor(private postsRepository: PostsRepository) {
 
-export const postsService = {
+    }
     async getPosts(page: number, pageSize: number) {
-        return await postsRepository.getPosts( page, pageSize )
-    },
+        return await this.postsRepository.getPosts(page, pageSize)
+    }
     async getPostsById(id: string) {
-        return await postsRepository.getPostsById(id)
-    },
+        return await this.postsRepository.getPostsById(id)
+    }
     async deletePostsById(id: string) {
-        return await postsRepository.deletePostsById(id)
-    },
+        return await this.postsRepository.deletePostsById(id)
+    }
     async updatePostsById(id: string, name: string, updatePost: NewPost) {
-            const bloggerName = name
-            return await postsRepository.updatePostsById({id, bloggerName, ...updatePost})
-    },
+        const bloggerName = name
+        return await this.postsRepository.updatePostsById({id, bloggerName, ...updatePost})
+    }
     async createPosts(newPost: NewPost) {
-        const blogger = await bloggersService.getBloggersById(newPost.bloggerId)
-            if(!blogger) {
-                return false
-            }
         const createPost = {
             ...newPost,
-            bloggerName: blogger.name,
             id: v4()
         }
-        return await postsRepository.createPosts(createPost)
-    },
+        return await this.postsRepository.createPosts(createPost)
+    }
     async getPostsInPages(bloggerId: string, page: number, pageSize: number) {
-        const post = await postsRepository.getPostInPages(bloggerId, page, pageSize)
+        const post = await this.postsRepository.getPostInPages(bloggerId, page, pageSize)
         return post
     }
 
+}
+export interface IPostsRepository {
+    getPosts(page: number, pageSize: number): Promise<Paginator<PostsCon[]>>
+    getPostsById(id: string): Promise<PostsCon | false>
+    deletePostsById(id: string): Promise<boolean>
+    updatePostsById(updatePost: PostsCon): Promise<Boolean>
+    createPosts(newPost: NewPost): Promise<PostsCon | null>
+    getPostInPages(bloggerId: string, page: number, pageSize: number): Promise<Paginator<PostsCon[]>>
 }
