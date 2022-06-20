de65ca09e52af13a18041e50c203632358ad71df
Alex <lunoface@mail.ru>
2022-06-20 14:41:15 +0700

    use ioc, fix pagination

--- src/repositories/users-repo.ts
+++ src/repositories/users-repo.ts
@@ -1,9 +1,18 @@
-import {Paginator, Users, usersCollection} from "./db";
+import {Paginator, usersCollection, UserType} from "./db";
+import {injectable} from "inversify";
+import * as MongoClient from "mongodb";
+import {v4} from "uuid";
+import {addHours} from "date-fns";
+import {IUsersRepository} from "../domain/users-service";
 
 
-export const usersRepo = {
-    async getUsers(page: number, pageSize: number) {
-        const user: Users[] = await usersCollection
+@injectable()
+export class UsersRepository implements IUsersRepository {
+    constructor(private usersCollection: MongoClient.Collection<UserType>) {
+    }
+
+    async getUsers(page: number, pageSize: number): Promise<Paginator<UserType[]>> {
+        const user = await this.usersCollection
             .find({}, {projection: {_id: 0, passwordHash: false}})
             .limit(pageSize)
             .skip((page - 1) * pageSize)
@@ -11,37 +20,68 @@ export const usersRepo = {
         const total = await usersCollection.countDocuments({})
         const pages = Math.ceil(total / pageSize)
 
-        const userInPages: Paginator<Users> = {
+        return ({
             pagesCount: pages,
             page: page,
             pageSize: pageSize,
             totalCount: total,
             items: user
-        }
-        return userInPages
-    },
+        })
+    }
 
-    async createUser(newUser: Users) {
-        await usersCollection.insertOne(newUser)
-        const createUser = await usersCollection.findOne({id: newUser.id})
-        return {
-            id: createUser!.id,
-            login: createUser!.login
-        }
-    },
+    async createUser(newUser: UserType): Promise<UserType | null> {
+        await this.usersCollection.insertOne(newUser)
+        const createdUser = await this.usersCollection.findOne({"accountData.id": newUser.accountData.id})
+        return createdUser
+            ? createdUser
+            : null
+    }
 
     async findByLogin(login: string) {
-        const user = await usersCollection.findOne({login})
+        const user = await this.usersCollection.findOne({login})
         return user
-    },
+    }
 
     async findById(id: string) {
-        const user = await usersCollection.findOne({id})
+        const user = await this.usersCollection.findOne({id})
         return user
-    },
+    }
 
     async delUser(id: string) {
-        const result = await usersCollection.deleteOne({id})
+        const result = await this.usersCollection.deleteOne({id})
         return result.deletedCount === 1
+    }
+    async findByEmail(email: string) {
+        const user = await this.usersCollection.findOne({"accountData.email": email})
+        return user
+    }
+    async findByConfirmCode(code: string) {
+        const user = await this.usersCollection.findOne({"emailConfirm.confirmationCode": code})
+        return user
+    }
+    async updateConfirm(id: string) {
+        const result = await this.usersCollection.findOneAndUpdate(
+            {id}, {
+                $set: {
+                    "emailConfirm.isConfirmed": true
+                }
+            })
+        return result.value
+    }
+    async updateConfirmCode(id: string) {
+        let update = await this.usersCollection.findOneAndUpdate({
+        'accountData.id': id}, {
+            $set: {
+                "emailConfirm.confirmationCode": v4(),
+                "emailConfirm.expirationDate": addHours(new Date(), 24)
+            }
+        }, {returnDocument: 'after'})
+        return update.value
+    }
+    async findExistingUser(login: string, email: string) {
+        const user = await this.usersCollection.findOne({
+            $or: [{'accountData.login': login}, {'accountData.email': email}]
+        })
+        return user
     }
 }
