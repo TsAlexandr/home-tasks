332651423d0657395e765e6e9b3e16af74a90a75
Alex <lunoface@mail.ru>
2022-06-21 00:34:18 +0700

    fix send message to user 10/11

--- src/application/email-scheduler.ts
+++ src/application/email-scheduler.ts
@@ -4,25 +4,17 @@ import {injectable} from "inversify";
 
 @injectable()
 export class EmailScheduler {
-    private _isRunning: boolean
     constructor(private emailService: EmailService) {
-        this._isRunning = false
-    }
-    public get isRunning() {
-        return this._isRunning
     }
     async emailSendRunning() {
-        this._isRunning = true
         const sendMail = await notificationRepository.dequeueMessage()
         if (sendMail) {
             setTimeout(async () => {
                 let error = await this.emailService.sendEmail(sendMail.email, sendMail.subject, sendMail.message)
-                console.log(error)
+                console.log("error: ", error)
                 await notificationRepository.updateMessage(sendMail._id)
                 await this.emailSendRunning()
             }, 1000)
-        }else{
-            this._isRunning = false
         }
     }
 }
