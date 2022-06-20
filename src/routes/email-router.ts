2e35ffd94d75969b4e53cacbd5f42206873fd366
Alex <lunoface@mail.ru>
2022-06-20 14:32:19 +0700

    fix import

--- src/routes/email-router.ts
+++ src/routes/email-router.ts
@@ -1,12 +1,12 @@
 import {Request, Response, Router} from "express";
-import {emailService} from "../domain/email-service";
+import {emailService} from "../iocContainer";
 
 
 export const emailRouter = Router({})
 
 emailRouter
-    .post('/send', async (req:Request, res: Response) => {
-        const {email, message, subject } = req.body
+    .post('/send', async (req: Request, res: Response) => {
+        const {email, message, subject} = req.body
         await emailService.sendEmail(email, message, subject)
 
     })
