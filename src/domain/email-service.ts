c30b1790ebe6499b6b896bf2e6b0e31dcb5189e5
Alex <lunoface@mail.ru>
2022-06-21 17:35:28 +0700

    fix yandex smtp 10/11

--- src/domain/email-service.ts
+++ src/domain/email-service.ts
@@ -8,27 +8,29 @@ export const templateService = {
 
 }
 
+let transporter = nodemailer.createTransport({
+    service: "Yandex" || "smtp.yandex.ru",
+    auth: {
+        user: "agerb3r@yandex.ru", // generated ethereal user
+        pass: "blbyf[eq15", // generated ethereal password
+    },
+});
+
+
 @injectable()
 export class EmailService {
     async sendEmail(email: string, subject: string, message: string) {
-        let transporter = nodemailer.createTransport({
-            service: "Yandex" || "smtp.yandex.ru",
-            auth: {
-                user: "agerb3r@yandex.ru", // generated ethereal user
-                pass: "blbyf[eq15", // generated ethereal password
-            },
-        })
+
+
 
         // send mail with defined transport object
-        try {
-            await transporter.sendMail({
+
+            return await transporter.sendMail({
                 from: 'Alex Gerber', // sender address
                 to: email, // list of receivers
                 subject: subject, // Subject line
                 html: message, // html body
             })
-        }catch (err) {
-            console.log("sendMail : ", err)
-        }
+
     }
 }
